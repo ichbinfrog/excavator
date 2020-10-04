295e9492a90fb4b42a3f88624665db5b25f9476d
Quoc Trung Hoang <quoc-trung.hoang@etu.utc.fr>
2020-10-04 18:51:27 +0200

    Update README
    Signed-off-by: Quoc Trung Hoang <quoc-trung.hoang@etu.utc.fr>

--- pkg/model/ruleset_test.go
+++ pkg/model/ruleset_test.go
@@ -1,10 +1,7 @@
 package model
 
 import (
-	"archive/tar"
 	"fmt"
-	"io"
-	"os"
 	"testing"
 )
 
@@ -12,24 +9,4 @@ func TestRuleMarshal(t *testing.T) {
 	rs := RuleSet{}
 	rs.ParseConfig("../../resources/rules.yaml")
 	fmt.Printf("%+v\n", rs)
-}
-
-func TestArchive(t *testing.T) {
-	fd, err := os.Open("tests/test_archive.tar")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	tarReader := tar.NewReader(fd)
-	for {
-		header, err := tarReader.Next()
-		if err == io.EOF {
-			break
-		}
-		fmt.Println(header)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-	}
 }
